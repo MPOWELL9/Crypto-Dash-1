a64133b9266e44c2b02573be0795f170eee91990
MPOWELL9 <mckaylaapowell@gmail.com>
2024-10-06 12:28:11 -0400

    react app

---
+++ crypto-react-app/src/App.js
@@ -0,0 +1,13 @@
+import './styles/cryptoCards.css';
+import CryptoDashboard from './components/CryptoDashboard';
+import React from 'react';
+
+function App() {
+  return (
+    <React.Fragment>
+       <CryptoDashboard/>
+    </React.Fragment>
+  );
+}
+
+export default App;
